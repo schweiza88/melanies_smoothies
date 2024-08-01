4591dbfce1423318a4fb5e2faa0051b169d1c1d0
Aaron Schweizer <aaron.schweizer@eaton.com>
2024-08-01 12:07:16 -0400

    Update streamlit_app.py
    Changed connection settings from SiS to SNiS.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -41,3 +40,6 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
+        
+cnx = st.connection("snowflake")
+session = cnx.session()
