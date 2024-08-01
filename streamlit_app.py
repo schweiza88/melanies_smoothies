93248576ef59c776b7afa6d8dd076dfcd7ad9127
Aaron Schweizer <aaron.schweizer@eaton.com>
2024-08-01 13:49:38 -0400

    Display nutritional data for each selected ingredient
    Display Fruityvice data for each chosen ingredient.

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-
+        #Fruityvice API call
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
     st.write(ingredients_string)
 
     my_insert_stmt = """insert into smoothies.public.orders(ingredients,name_on_order)
@@ -44,6 +46,3 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
         
-#Fruityvice API call
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
