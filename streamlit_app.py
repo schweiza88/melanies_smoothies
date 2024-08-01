996cb42e6da9aec41fbaddfedeecad5ac4381ce7
Aaron Schweizer <aaron.schweizer@eaton.com>
2024-08-01 13:46:39 -0400

    Display fruityvice data in app
    Display fruityvice HTTP response within the application markup.

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,4 +46,4 @@ if ingredients_list:
         
 #Fruityvice API call
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
