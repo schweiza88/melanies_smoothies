344a347b57430aaa1c49b96bd6f31574264c8fb9
Aaron Schweizer <aaron.schweizer@eaton.com>
2024-08-01 12:43:06 -0400

    Update streamlit_app.py
    Corrected the SniS connection information. Pasted new connection details out of required scope.

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,10 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
+cnx = st.connection("snowflake")
+session = cnx.session()
 session = get_active_session()
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
@@ -41,5 +44,3 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
         
-cnx = st.connection("snowflake")
-session = cnx.session()
